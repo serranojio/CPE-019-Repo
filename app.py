d665041560d6d1f9f67f6d2a92b77da7d6763483
serranojio <157586862+serranojio@users.noreply.github.com>
2024-05-17 21:31:39 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import tensorflow as tf
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model=tf.keras.models.load_model('final_model.h5')
+  return model
+model=load_model()
+st.write("""
+# Egg Quality Detection System"""
+)
+file=st.file_uploader("Choose an egg photo from computer",type=["jpg","png"])
+
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['Cracked', 'None', 'Good']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
